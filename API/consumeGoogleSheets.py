ee658d96804a9b77e8660808ea171148344f9b0e
MarcosViniSil <Marcossilv203@gmail.com>
2024-04-19 19:31:48 -0300

    working with enviorments

--- API/consumeGoogleSheets.py
+++ API/consumeGoogleSheets.py
@@ -1,15 +1,10 @@
 import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 def insertDatasCoins(datas):
-    url = 'https://script.google.com/macros/s/AKfycbycSeOU7amm20Xns6RxscpSa8FElMMRpXigbuFwugcVKhPZpiTFD2OLCVLjswnniuFJ/exec'
-    response = requests.post(url, data=datas)
-    if response.status_code == 200:
-        print("Requisição POST bem-sucedida!")
-    else:
-        print("Erro na requisição:", response.status_code)
-
-def insertDatasCrypto(datas):
-    url = 'https://script.google.com/macros/s/AKfycbyKNakGtxRlCR1VZkBHTsrx0W8T6zj4nz6_s8bvmWsulqvpjy0XFWMURPiL9w5jbb0QnQ/exec'
+    url = os.environ["url"]
     response = requests.post(url, data=datas)
     if response.status_code == 200:
         print("Requisição POST bem-sucedida!")
