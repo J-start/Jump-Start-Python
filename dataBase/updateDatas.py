fdcca1da603bf0d7a72f0544f504c849d4e0464e
MarcosViniSil <Marcossilv203@gmail.com>
2024-04-23 18:20:12 -0300

    code refactoring

--- dataBase/updateDatas.py
+++ dataBase/updateDatas.py
@@ -35,39 +35,34 @@ def deleteAssets(table,asset):
 
 
 def fetchPossiblesIdsToDelete(table,asset,idToDelete):
+        N=1
         countCrypto = getCountAsset(table,asset)
-        if countCrypto > 1:
+        if countCrypto > N:
             idCrypto = getIdToDeleteAsset(table,asset)
             idToDelete.append(idCrypto)   
         return idToDelete 
 
+def fetchEachAsset(table,assets):
+    idToDelete = []
+    for asset in assets:
+        idToDelete=fetchPossiblesIdsToDelete(table,asset,idToDelete)
+    if idToDelete != []:
+        deleteAssets(table,idToDelete)
+
 def manipulationCryptos():
     cryptos = ["BTC","LTC","ETH","XRP","BCH","USDT","LINK","DOGE","ADA","EOS","XLM","CHZ","AXS"]
-    idToDelete = []
-    for crypto in cryptos:
-        idToDelete=fetchPossiblesIdsToDelete(table="tb_crypto",asset=crypto,idToDelete=idToDelete)
-    if idToDelete != []:
-        deleteAssets("tb_crypto",idToDelete)
+    fetchEachAsset("tb_crypto",cryptos)
 
 def manipulationAcoes():
     actions = ["PETR4.SA","BBAS3.SA","ITSA4.SA","TRPL4.SA","VALE3.SA","CMIG4.SA","SANB11.SA","USIM5.SA","ABEV3.SA","MGLU3.SA"]
-    idToDelete = []
-    for action in actions:
-        idToDelete=fetchPossiblesIdsToDelete(table="tb_acoes",asset=action,idToDelete=idToDelete)
-    if idToDelete != []:
-        deleteAssets("tb_acoes",idToDelete)
+    fetchEachAsset("tb_acoes",actions)
 
 def manipulationCoins():
     coins = ["AED","ARS","AUD","BOB","CAD","CHF","CLP","CNY","COP","DKK","EUR","GBP","HKD","ILS","INR","JPY","MXN","NOK","NZD","PEN","PLN","PYG","RUB","SAR","SEK","SGD","THB","TRY","TWD","USD","USDT","UYU","VEF","XRP","ZAR"]
-    idToDelete = []
-    for coin in coins:
-        idToDelete=fetchPossiblesIdsToDelete(table="tb_coins",asset=coin,idToDelete=idToDelete)
-    if idToDelete != []:
-        deleteAssets("tb_coins",idToDelete)
-
+    fetchEachAsset("tb_coins",coins)
 
 def main():
-    manipulationCoins()
+    manipulationCryptos()
 
 if __name__ == "__main__":
     main()
