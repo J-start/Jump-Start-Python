32d13f2d975bee689aefa3cfb8e30718c566c269
MarcosViniSil <Marcossilv203@gmail.com>
2024-04-26 17:56:54 -0300

    configuration schedule

--- API/scheduleConsumeApi.py
+++ API/scheduleConsumeApi.py
@@ -4,37 +4,44 @@ from manipulation.cryptocurrency import *
 from manipulation.acoes import *
 import sched
 import time
+import threading
 
 scheduler = sched.scheduler(time.time, time.sleep)
 
-def scheduleCoin():
-    scheduler.enter(900, 1, scheduleCoin)
-    getAllCoinsAndPrint()
+TIMESCHEDULE = 1800
 
-def executeScheduleCoin():
-    scheduleCoin()
+
+def execute():
+    begin = time.time()
+
+    thread_coin = threading.Thread(target=getAllCoinsAndPrint("jumpstart"))
+    thread_selic = threading.Thread(target=manipulationSelic("jumpstart"))
+    thread_crypto = threading.Thread(target=getAndPrintAllCryptos("jumpstart"))
+    thread_action = threading.Thread(target=fetchAllInformationActions("jumpstart"))
+
+    thread_coin.start()
+    thread_selic.start()
+    thread_crypto.start()
+    thread_action.start()
+
+    thread_coin.join()
+    thread_selic.join()
+    thread_crypto.join()
+    thread_action.join()
+    
+    end = time.time()
+    timeFunction = end - begin
+
+    print("time: ",timeFunction)
+
+def schedule():
+    scheduler.enter(TIMESCHEDULE, 1, schedule)
+    execute()
+
+def executeSchedule():
+    schedule()
     scheduler.run()
 
-def scheduleSelic():
-    scheduler.enter(900, 1, scheduleSelic)
-    manipulationSelic()
 
-def executeScheduleSelic():
-    scheduleSelic()
-    scheduler.run()
 
-def scheduleCrypto():
-    scheduler.enter(900, 1, getAndPrintAllCryptos)
-    getAndPrintAllCryptos()
 
-def executeScheduleCrypto():
-    scheduleCrypto()
-    scheduler.run()
-
-def scheduleAction():
-    scheduler.enter(900, 1, fetchAllInformationActions)
-    fetchAllInformationActions()
-
-def executeScheduleAction():
-    scheduleAction()
-    scheduler.run()
--- main.py
+++ main.py
@@ -6,35 +6,7 @@ from manipulation.acoes import *
 from api.consumeGoogleSheets import *
 from dataBase.insertDatas import *
 
-import time
-import threading
-
-
-inicio = time.time()
-
-thread_coin = threading.Thread(target=getAllCoinsAndPrint("jumpstart"))
-thread_selic = threading.Thread(target=manipulationSelic("jumpstart"))
-thread_crypto = threading.Thread(target=getAndPrintAllCryptos("jumpstart"))
-thread_action = threading.Thread(target=fetchAllInformationActions("jumpstart"))
-
-thread_coin.start()
-thread_selic.start()
-thread_crypto.start()
-thread_action.start()
-
-thread_coin.join()
-thread_selic.join()
-thread_crypto.join()
-thread_action.join()
-
-#getAllCoinsAndPrint()
-#manipulationSelic()
-#getAndPrintAllCryptos()
-#fetchAllInformationActions()
-
-fim = time.time()
-timeFunction = fim - inicio
-print("time: ",timeFunction)
+executeSchedule()
 
 # With threads => 33s
 # WithOut threads => 46s
